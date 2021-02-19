d26d48c9e1dea77392dc0d9484bd101e09ed583f
hukurou82 <hukurou82@gmail.com>
2021-02-19 14:07:33 +0900

    reducerの削除機能の追加

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useReducer } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Event from "./Event";
 import reducer from "../reducers";
 
 const App = () => {
@@ -19,8 +20,6 @@ const App = () => {
     setTitle("");
     setBody("");
   };
-
-  console.log({ state });
 
   return (
     <div className="container-fluid">
@@ -62,6 +61,11 @@ const App = () => {
             <th></th>
           </tr>
         </thead>
+        <tbody>
+          {state.map((event, index) => (
+            <Event key={index} event={event} dispatch={dispatch} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
