0c000bd15e649ce79c6de5c8b8c49577bcc490ff
hukurou82 <hukurou82@gmail.com>
2021-02-24 17:02:02 +0900

    ローカルに入力したデータ（state）を保存する記述

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,12 +9,25 @@ import OperationLogs from "./OperationLogs";
 import AppContext from "../contexts/AppContext";
 import reducer from "../reducers";
 
+const APP_KEY = "appWithRedux";
+
 const App = () => {
-  const initialState = {
-    events: [],
-    operationLogs: [],
-  };
+  const appState = localStorage.getItem(APP_KEY);
+
+  //Javascript型にしないと参照出来ないため、JSON.parseでappStateに入っている文字列をオブジェクトに戻している。
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
+
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    //JSON.stringifyは全てを文字列にするメソッド
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
